dc7a60b4397eb20ee462f59a21d24cc21b9309ab
Cristoffer <119351534+Cristoffer85@users.noreply.github.com>
2023-03-15 11:54:04 +0100

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Pseudokod
+        System.out.println("Kalle har 10 äpplen");
+        System.out.println("Pelle har 15 äpplen");
+    // Vi använder int för att vi arbetar med heltal
+
+        //Deklarera
+        int kalle = 10; // variabler ska alltid skrivas med camelCase
+        int pelle = 15; // variabler ska alltid skrivas med camelCase
+        int tillsammans = kalle+pelle;
+        System.out.println("Tillsammans har de " + tillsammans + " äpplen");
+        System.out.println("Vi vet ännu inte var de har tagit äpplena ifrån :(");
+        // Beräkna
+
+        // Skriv ut
+    }
+}
